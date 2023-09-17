dadc73e1b8ef3c093ff1b17c4bb4525db2098eed
Bruce Green <bruce@bwgz.org>
2023-09-16 20:23:23 -0500

    Initial base build

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1>Hello World!</h1>
+      <nav>
+      </nav>
+    </div>
+  </header>
+
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import "@trimbleinc/modus-bootstrap/dist/modus.css"
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
