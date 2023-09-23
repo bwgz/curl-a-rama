94b83a387242918e8ac991470740b46627371c6e
Bruce Green <bruce@bwgz.org>
2023-09-22 19:10:16 -0500

    no more client secret in code

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,8 +1,6 @@
 
 const client_secret = import.meta.env.VITE_CLIENT_SECRET;
 
-console.log("client_secret", client_secret);
-
 export const oidcSettings = {
     authority: 'https://accounts.google.com',
     client_id: '260770349013-ks1orjk0p0uohurl0f9phc3ed0pkqg1g.apps.googleusercontent.com', // client_id if you use typescript, see note below
