b174147d80a160f26c7d538b8e4ec95ea33f681e
Bruce Green <bruce@bwgz.org>
2023-09-22 19:49:10 -0500

    env changes

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,11 +1,14 @@
 
 const client_secret = import.meta.env.VITE_CLIENT_SECRET;
+const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
+
+console.log('redirect_uri', redirect_uri);
 
 export const oidcSettings = {
     authority: 'https://accounts.google.com',
     client_id: '260770349013-ks1orjk0p0uohurl0f9phc3ed0pkqg1g.apps.googleusercontent.com', // client_id if you use typescript, see note below
     client_secret,
-    redirect_uri: 'http://localhost:5173/curl-a-rama/callback', // client_id if you use typescript, see note below
+    redirect_uri,
     responseType: 'code',
     scope: 'openid email profile',
   }
